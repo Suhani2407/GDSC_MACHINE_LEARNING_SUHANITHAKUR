62f498c342f2cadcb002f61c81bb551e4df33901
Suhani2407 <124596202+Suhani2407@users.noreply.github.com>
2023-12-26 10:52:08 +0530

    Add files via upload

---
+++ Cat_Dog.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+'''
+# Define the CNN model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))  # Binary classification (dog or cat)
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Data Preprocessing using ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(r'C:/Users/suhan/OneDrive/Desktop/Recipe/PetImages', target_size=(150, 150), batch_size=32,class_mode='binary')
+
+# Train the model
+model.fit(train_generator, epochs=10)
+
+# Save the model
+model.save('dog_cat_classifier.h5')
+'''
+
+from keras.optimizers import RMSprop
+
+# Define the CNN model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))  # Binary classification (dog or cat)
+
+rmsprop= RMSprop(learning_rate=0.01, rho=0.9)
+
+# Compile the model
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Data Preprocessing using ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(r'C:/Users/suhan/OneDrive/Desktop/Recipe/PetImages', target_size=(150, 150), batch_size=32,class_mode='binary')
+
+# Train the model
+model.fit(train_generator, epochs=10)
+
+# Save the model
+model.save('dog_cat_classifier1.h5')
